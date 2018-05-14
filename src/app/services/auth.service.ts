684b73f4a0b848e5b87c64f3cba7022fec879aff
Oerd Cukalla <oerd@users.noreply.github.com>
2018-05-15 00:32:46 +0200

    Update `auth.service.ts` imports to recent rxjs
    most recent angular cli will not find `'rxjs/add/operator/toPromise'` as this is ES6 functionality.
    See http://reactivex.io/rxjs/file/es6/add/operator/toPromise.js.html

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import { User } from '../models/user';
-import 'rxjs/add/operator/toPromise';
+import 'es6/add/operator/toPromise';
 
 @Injectable()
 export class AuthService {
